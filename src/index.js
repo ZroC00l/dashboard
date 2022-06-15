8ead85440cbb955f858e4c4de051e060f4d585b6
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-15 12:44:26 +0200

    tailwindcss configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./index.css";
 import App from "./App";
+//This will import all Tailwind CSS styles
 
 ReactDOM.render(<App />, document.getElementById("root"));
