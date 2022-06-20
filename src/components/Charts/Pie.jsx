24956d0b15d710115aeb4db23560b40d9e2e6ed4
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-20 09:44:32 +0200

    stacked component update

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  AccumulationLegend,
+  PieSeries,
+  AccumulationDataLabel,
+  Inject,
+  AccumulationTooltip,
+} from "@syncfusion/ej2-react-charts";
+import { useStateContext } from "../../contexts/ContextProvider";
 
-const Pie = () => {
-  return <div>Pie</div>;
+const Doughnut = ({ id, data, legendVisiblity, height }) => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <AccumulationChartComponent
+      id={id}
+      legendSettings={{ visible: legendVisiblity, color: "white" }}
+      height={height}
+      background={currentMode === "dark" ? "#33373E" : "#fff"}
+      tooltip={{ enable: true }}
+    >
+      <Inject
+        services={[
+          AccumulationLegend,
+          AccumulationTooltip,
+          PieSeries,
+          AccumulationDataLabel,
+        ]}
+      />
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          name="Scale"
+          dataSource={data}
+          xName="x"
+          yName="y"
+          innerRadius="40%"
+          startAngle={0}
+          endAngel={360}
+          radius="70%"
+          explode
+          explodeOffset="10%"
+          explodeIndex={2}
+          dataLabel={{
+            visible: true,
+            name: "text",
+            position: "Inside",
+            font: {
+              fontWeight: "600",
+              colot: "#fff",
+            },
+          }}
+        />
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
+  );
 };
 
-export default Pie;
+export default Doughnut;
