421898f7c6898dc7258a0d2d5b822ad56de68d68
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-20 08:50:40 +0200

    SparkLine component successfuly rendered

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
-};
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
+
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        valueType="Numeric"
+        lineWidth={1}
+        tooltipSettings={{
+          visible: true,
+          format: "${x} : data ${yval}",
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        markerSettings={{
+          visible: ["All"],
+          size: 2.5,
+          fill: currentColor,
+        }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
+  }
+}
 
 export default SparkLine;
