808aa9d166c485a1ec346dd62b5f14ebf3603f64
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-20 07:49:49 +0200

    chartsHeader component

--- src/components/ChartsHeader.jsx
+++ src/components/ChartsHeader.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const ChartsHeader = () => {
-  return <div>ChartsHeader</div>;
+const ChartsHeader = ({ category, title }) => {
+  return (
+    <div className="mb-4">
+      <div>
+        <p className="text-lg text-gray-400">Chart</p>
+        <p className="text-3xl font-extrabold tracking-tight dark:text-gray-200 text-slate-90">
+          {category}
+        </p>
+      </div>
+      <p className="text-center dark:text-gray-200 text-xl mb-2 mt-3">
+        {title}
+      </p>
+    </div>
+  );
 };
 
 export default ChartsHeader;
