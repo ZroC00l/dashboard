0d444b615c21914af0c3086c4f06c739c4815056
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-21 08:39:28 +0200

    Customers component grid layout

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
-  return <div>Customer</div>;
+  const editing = { allowDeleting: true, allowEditing: true };
+  const toolbarOptions = ["Delete"];
+  const selectionsettings = { persistSelection: true };
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <GridComponent
+        id=""
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        editSettings={editing}
+        toolbar={toolbarOptions}
+        selectionSettings={selectionsettings}
+      >
+        <Inject services={[Page, Selection, Edit, Toolbar, Sort, Filter]} />
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customers;
