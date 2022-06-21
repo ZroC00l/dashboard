ba116aef134413f1ef485a01f6d62a455cc87d65
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-21 09:05:37 +0200

    calender component drag and drop event feature

--- src/pages/Calender.jsx
+++ src/pages/Calender.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ScheduleComponent,
   ViewsDirective,
@@ -17,18 +17,59 @@ import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
 import { scheduleData } from "../data/dummy";
 import { Header } from "../components";
 
-const propertyPane = (props) => <div className="mt-5">{props.children}</div>;
+const PropertyPane = (props) => <div className="mt-5">{props.children}</div>;
 
 const Scheduler = () => {
+  const [scheduleObj, setScheduleObj] = useState();
+
+  const change = (args) => {
+    scheduleObj.selectedDate = args.value;
+    scheduleObj.dataBind();
+  };
+
+  const onDragStart = (args) => {
+    args.navigation.enable = true;
+  };
+
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Calender" />
-      <ScheduleComponent>
-        <ViewsDirective></ViewsDirective>
+      <ScheduleComponent
+        eventSettings={{ dataSource: scheduleData }}
+        height="650px"
+        selectedDate={new Date(2022, 0, 21)}
+        ref={(schedule) => {
+          setScheduleObj(schedule);
+        }}
+        dragStart={onDragStart}
+      >
+        <ViewsDirective>
+          {["Day", "Week", "WorkWeek", "Month", "Agenda"].map((item) => (
+            <ViewDirective key={item} option={item} />
+          ))}
+        </ViewsDirective>
         <Inject
           services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
         />
       </ScheduleComponent>
+
+      <PropertyPane>
+        <table style={{ width: "100%", background: "white" }}>
+          <tbody>
+            <tr style={{ height: "50px" }}>
+              <td style={{ width: "100%" }}>
+                <DatePickerComponent
+                  value={new Date(2022, 0, 21)}
+                  showClearButton={false}
+                  placeholder="Current date"
+                  floatLabelType="Always"
+                  change={change}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </PropertyPane>
     </div>
   );
 };
