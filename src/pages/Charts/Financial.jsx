bb13bd1c974b09182ae9cb415f93b9d86fd34c50
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-21 12:07:38 +0200

    financial graph successfully rendered

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,7 +1,75 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  HiloSeries,
+  Tooltip,
+  DateTime,
+  Zoom,
+  Logarithmic,
+  Crosshair,
+} from "@syncfusion/ej2-react-charts";
+import {
+  financialChartData,
+  FinancialPrimaryXAxis,
+  FinancialPrimaryYAxis,
+} from "../../data/dummy";
+import { ChartsHeader } from "../../components";
+import { useStateContext } from "../../contexts/ContextProvider";
+
+const date1 = new Date("2017, 1, 1");
+
+function filterValue(value) {
+  if (value.x >= date1) {
+    return value.x, value.high, value.low;
+  }
+}
+
+const returnValue = financialChartData.filter(filterValue);
 
 const Financial = () => {
-  return <div>Financial</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartsHeader category="Financial" title="APPLE Historical" />
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: "Vertical", line: { width: 0 } }}
+          chartArea={{ border: { width: 0 } }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+        >
+          <Inject
+            services={[
+              Tooltip,
+              DateTime,
+              Zoom,
+              Logarithmic,
+              HiloSeries,
+              Crosshair,
+            ]}
+          />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={returnValue}
+              xName="x"
+              yName="low"
+              type="Hilo"
+              name="Apple inc."
+              low="low"
+              high="high"
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
 };
 
 export default Financial;
